72e9e3563ded04e6cf577588206b31e1049fa20f
Jason <DuckBoss@users.noreply.github.com>
2017-05-19 19:31:56 -0400

    Create EvenFibonacciNumbers.java

---
+++ Even_Fibonacci_Numbers/EvenFibonacciNumbers.java
@@ -0,0 +1,32 @@
+
+public class EvenFibonacciNumbers {
+	public static void main(String[] args) {
+		
+		if(args.length != 3) {
+			System.out.println("This program requires 2 starting integers, and the max terms to count!");
+			System.exit(0);
+		}
+		
+		int start1 = Integer.parseInt(args[0]);
+		int start2 = Integer.parseInt(args[1]);
+		int maxCount = Integer.parseInt(args[2]);
+		if(maxCount < 2)
+			maxCount = 2;
+		
+		int[] fullList = new int[maxCount];
+		fullList[0] = start1;
+		fullList[1] = start2;
+		
+		for(int i = 2; i < maxCount; i++)
+			fullList[i] = fullList[i-1]+fullList[i-2];
+		
+		System.out.print("Result - {");
+		for(int i = 0; i < fullList.length; i++)
+			if(fullList[i]%2 == 0)
+				System.out.print(String.format(" %d", fullList[i]));
+		System.out.print(" }\n");
+		
+		System.out.println();
+		
+	}
+}
