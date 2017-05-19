6448d33dad10c664e20cdb57e556e0cb82f5258d
Jason <DuckBoss@users.noreply.github.com>
2017-05-19 04:59:07 -0400

    Create FibonacciNumbersSum.cs

---
+++ Fibonacci_Numbers_Sum/FibonacciNumbersSum.cs
@@ -0,0 +1,45 @@
+using System;
+
+namespace FibonacciNumbers
+{
+	class MainClass
+	{
+		public static void Main(string[] args)
+		{
+			if (args.Length != 3)
+			{
+				Console.WriteLine("This program requires 2 starting integers, and the max terms to count!\n");
+				Environment.Exit(0);
+			}
+
+			int start1 = Convert.ToInt32(args[0]);
+			int start2 = Convert.ToInt32(args[1]);
+			int maxCount = Convert.ToInt32(args[2]);
+			if (maxCount < 2)
+				maxCount = 2;
+
+			int[] fullList = new int[maxCount];
+			int sum = 0;
+			fullList[0] = start1;
+			fullList[1] = start2;
+
+			for (int i = 2; i < maxCount; i++)
+				fullList[i] = fullList[i - 1] + fullList[i - 2];
+
+			Console.Write("Result - {");
+			foreach (int fib in fullList)
+			{
+				Console.Write($" {fib}");
+				sum += fib;
+			}
+			Console.Write(" }\n");
+
+
+			Console.WriteLine($"Sum - {sum}");
+
+
+			Console.WriteLine();
+			Console.ReadKey();
+		}
+	}
+}
