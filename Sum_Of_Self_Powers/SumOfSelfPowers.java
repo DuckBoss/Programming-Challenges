80db9815083953668afe1e533c3a90c4e9c060a2
Jason <DuckBoss@users.noreply.github.com>
2017-05-19 22:23:14 -0400

    Create SumOfSelfPowers.java

---
+++ Sum_Of_Self_Powers/SumOfSelfPowers.java
@@ -0,0 +1,23 @@
+
+public class SelfPowers {
+
+	public static void main(String[] args) {
+		
+		if(args.length < 1) {
+			System.out.println("This program requires 1 integer input!");
+			System.exit(0);
+		}
+		
+		long totalSum = 0; 
+		int inputtedNum = Integer.parseInt(args[0]);
+		for(int i = 1; i <= inputtedNum; i++) {	
+			long val = (long) Math.pow(i, i);
+			totalSum += val;
+			System.out.println(val);
+		}
+		System.out.println("Total Sum - " + totalSum);
+		
+		
+	}
+
+}
