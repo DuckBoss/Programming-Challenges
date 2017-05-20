b5938d61b0952bf30f672e8d07a83049dc615d33
Jason <DuckBoss@users.noreply.github.com>
2017-05-20 05:47:43 -0400

    Create SumOfMultiples.cpp

---
+++ Sum_Of_Multiples/SumOfMultiples.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+#include <math.h>
+#include <vector>
+
+using namespace std;
+
+int main(int argc, char* argv[]) {
+
+	if (argc < 3) {
+		cout << "This program requires 1 integer input and 1 multiple input!" << endl;
+		cout << "NOTE: This program can handle multiple 'Multiple integer' inputs..." << endl;
+		exit(0);
+	}
+
+	int givenNum = atoi(argv[1]);
+	vector<int> givenMultiples;
+	for (int i = 2; i < argc; i++) {
+		givenMultiples.push_back(atoi(argv[i]));
+	}
+
+	
+	int totalSum = 0;
+	for (int g = 0; g < givenMultiples.size(); g++) {
+		int sum = 0;
+		for (int i = givenMultiples[g]; i < givenNum; i += givenMultiples[g]) {
+			sum += i;
+		}
+		totalSum += sum;
+	}
+
+	cout << endl;
+	cout << "Multiples Of - ";
+	for (int multiple : givenMultiples)
+	{
+		cout << multiple << " ";
+	}
+	cout << "under " << givenNum << endl;
+	cout << "Sum Of Multiples - " << totalSum << endl;
+	
+
+
+	return 0;
+}
