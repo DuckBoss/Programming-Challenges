de69909ad4179543653d3d8f7c3a05cfb51a8c20
Jason <DuckBoss@users.noreply.github.com>
2017-05-20 06:00:27 -0400

    Create FibonacciNumbers.cpp

---
+++ Fibonacci_Numbers/FibonacciNumbers.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main(int argc, char* argv[]) {
+
+	if (argc != 4) {
+		cout << "This program requires 2 starting integers, and the max terms to count!" << endl;
+		exit(0);
+	}
+
+	int start1 = atoi(argv[1]);
+	int start2 = atoi(argv[2]);
+	int maxCount = atoi(argv[3]);
+	if (maxCount < 2)
+		maxCount = 2;
+
+	vector<int> fullList;
+	fullList.push_back(start1);
+	fullList.push_back(start2);
+	fullList.resize(maxCount);
+
+	for (int i = 2; i < maxCount; i++) {
+		fullList[i] = fullList[i - 1] + fullList[i - 2];
+	}
+	cout << "Result - {";
+	for (int fib : fullList) {
+		cout << " " << fib;
+	}
+	cout << " }" << endl;
+
+	system("pause");
+	return 0;
+}
