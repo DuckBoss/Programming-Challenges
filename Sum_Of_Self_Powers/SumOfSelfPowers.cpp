2eb5860be614f8ec41d6bffcad6ff8dd65b51e13
Jason <DuckBoss@users.noreply.github.com>
2017-05-20 05:20:43 -0400

    Create SumOfSelfPowers.cpp

---
+++ Sum_Of_Self_Powers/SumOfSelfPowers.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <math.h>
+
+using namespace std;
+
+int main(int argc, char* argv[]) {
+	if (argc != 2) {
+		cout << "This program requires 1 integer input!" << endl;
+		exit(0);
+	}
+
+	long totalSum = 0;
+	int inputtedNum = atoi(argv[1]);
+	
+	for (int i = 1; i <= inputtedNum; i++) {
+		long val = (long)pow(i, i);
+		totalSum += val;
+		cout << val << endl;
+	}
+	cout << "Total Sum - " << totalSum <<endl;
+	system("pause");
+	return 0;
+}
